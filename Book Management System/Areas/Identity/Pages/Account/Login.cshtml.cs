25a3e350aec4a516a2d1540c4b2ad6dd9a8805c0
user <user@LEO-NB>
2025-01-25 22:58:23 +0800

    會員登入介面完成

--- Book Management System/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Book Management System/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -81,7 +81,7 @@ namespace Book_Management_System.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Keep me signed in")]
             public bool RememberMe { get; set; }
         }
 
